d88b327f6788ed67f2be16ed435bc0594d72ffb1
Marcos Vinicius <85797701+strngrthnall@users.noreply.github.com>
2023-05-09 23:58:59 -0300

    Responsividade e ajustes do hero

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const heroSection = document.querySelector('.hero')
   const alturaHero = heroSection.clientHeight
 
+  const buttons = document.querySelectorAll('[data-tab-button]')
+
   window.addEventListener('scroll', () => {
     const posicaoAtual = window.scrollY
 
@@ -11,6 +13,17 @@ document.addEventListener('DOMContentLoaded', () => {
       exibeElementosDoHeader()
     }
   })
+
+  for(let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', (botao) => {
+      const tabTarget = botao.target.dataset.tabButton
+      const tab = document.querySelector(`[data-tab-id=${tabTarget}]`)
+      hideAllTabs()
+      tab.classList.add('examples__list--is-active')
+      removeActiveButton()
+      botao.target.classList.add('examples__tabs__button--is-active')
+    })
+  }
 })
 
 function ocultaElementosDoHeader(){
@@ -21,4 +34,21 @@ function ocultaElementosDoHeader(){
 function exibeElementosDoHeader(){
   const header = document.querySelector('.header')
   header.classList.remove('header--is-hidden')
+}
+
+function hideAllTabs() {
+  const tabsContainer = document.querySelectorAll('[data-tab-id]')
+
+  for(let i = 0; i < tabsContainer.length; i++) {
+    tabsContainer[i].classList.remove('examples__list--is-active')
+  }
+
+}
+
+function removeActiveButton() {
+  const buttons = document.querySelectorAll('[data-tab-button]')
+
+  for(let i = 0; i < buttons.length; i++) {
+    buttons[i].classList.remove('examples__tabs__button--is-active')
+  }
 }
